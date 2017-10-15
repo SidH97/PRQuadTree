f38572a0312469574c51a3764bb15b1ac0229c73
Sid <sid97@vt.edu>
2017-10-15 16:31:14 -0400

    Added all other classes needed to finish up. testing is still to-do

---
+++ src/FileReader.java
@@ -0,0 +1,138 @@
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * This class will run our project.
+ *
+ * @author Siddharth Hingorani (sid97)
+ * @author Matthew Evans (matce93)
+ * @version 2017.09.21
+ *
+ */
+public class FileReader
+{
+    private Database dbase = new Database();
+
+    /**
+     * This is our default constructor
+     *
+     * @param fileName
+     *            the name to create from.
+     */
+    public FileReader(String fileName)
+    {
+        executeLines(fileName);
+    }
+
+    /**
+     * This method will run through and execute helper commands base on the
+     * input file.
+     * 
+     * @param in
+     *            the filename.
+     */
+
+    private void executeLines(String in)
+    {
+        Scanner scanner = null;
+        File file = new File(in);
+        try
+        {
+            scanner = new Scanner(file);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Cannot find given File Name!");
+            System.exit(0);
+        }
+        while (scanner.hasNextLine())
+        {
+            String line = scanner.nextLine();
+            Scanner lineScan = new Scanner(line);
+            String command = null;
+            if (lineScan.hasNext())
+            {
+                command = lineScan.next();
+                if (command.equals("insert"))
+                {
+                    try
+                    {
+                        String name = lineScan.next();
+                        int x = Integer.parseInt(lineScan.next());
+                        int y = Integer.parseInt(lineScan.next());
+                        dbase.insertPoint(name, x, y);
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("missing parameter for command "
+                                        + command);
+                    }
+                }
+                else if (command.equals("remove"))
+                {
+                    String word = null;
+                    try
+                    {
+                        word = lineScan.next();
+                        int x = Integer.parseInt(word);
+                        int y = Integer.parseInt(lineScan.next());
+                        dbase.removeBydimensions(x, y);
+                    }
+                    catch (Exception e)
+                    {
+                        dbase.removeByName(word);
+                    }
+
+                }
+                else if (command.equals("regionsearch"))
+                {
+                    try
+                    {
+                        int x = Integer.parseInt(lineScan.next());
+                        int y = Integer.parseInt(lineScan.next());
+                        int width = Integer.parseInt(lineScan.next());
+                        int height = Integer.parseInt(lineScan.next());
+                        dbase.regionSearch(x, y, width, height);
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("missing parameter for command "
+                                        + command);
+                    }
+                }
+                else if (command.equals("duplicates"))
+                {
+                    dbase.duplicates();
+                }
+                else if (command.equals("search"))
+                {
+                    try
+                    {
+                        String name = lineScan.next();
+                        dbase.search(name);
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("name not given");
+                    }
+                }
+                else if (command.equals("dump"))
+                {
+                    // TO DO
+                }
+                else
+                {
+                    System.out.println("");
+                }
+            }
+            else
+            {
+                lineScan.close();
+            }
+            lineScan.close();
+
+        }
+
+    }
+}
